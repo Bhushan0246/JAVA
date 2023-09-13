0ff3504b97f223e687318368f4394af9cd4080d6
Bhushan Harode <bhushanharode0246@gmail.com>
2023-09-13 18:00:39 +0530

    Add files via upload

--- MinMax.java
+++ MinMax.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
 class MinMax{
 	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Minimum and Maximum of five numbers.");
-		System.out.println("Array: {1, 8, 6, 4, 2}");
-		int arr[] = {1,8,6,4,2}, size = 5, min = 0, max = 0;
-		for(int i=0; i<(size-1); i++){	// check for minimum element
-			if(arr[i] < arr[i+1])
-				min = arr[i];
-		}
-		for (int i = 0; i < (size-1); i++) {	// check for maximum element
+		System.out.print("Enter five numbers: ");
+		int arr[] = new int[5], max = 0;
+		for(int i=0; i<arr.length; i++)
+			arr[i] = sc.nextInt();
+		for (int i = 0; i < arr.length; i++) {	// check for maximum element
 			if(arr[i] > max)
 				max = arr[i];
+		}
+		int min = max;
+		for(int i = 0; i<arr.length; i++){	// check for minimum element
+			if(arr[i]<min)
+				min = arr[i];
 		}
 		System.out.println("Minimum = " + min);
 		System.out.println("Maximim = " + max);
