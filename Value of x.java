9436bb8514aec1c6864b8758f11207dc7282ec2c
Bhushan Harode <bhushanharode0246@gmail.com>
2023-09-13 17:59:44 +0530

    Add files via upload

---
+++ Value of x.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Value_of_x{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = 7+3*6/2-1;
+        System.out.println("Value of x1 = " + x);
+        x = 2%2+2*2-2/2;
+        System.out.println("Value of x2 = " + x);
+        x = (3*9*(3+(9*3/(3))));
+        System.out.println("Value of x3 = " + x);
+    }
+}
