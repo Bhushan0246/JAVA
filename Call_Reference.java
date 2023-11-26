fb91cc07893e6aa316d2e0593a5f556b69077841
Bhushan Harode <bhushanharode0246@gmail.com>
2023-11-26 23:31:52 +0530

    Update Call_Reference.java

--- Call_Reference.java
+++ Call_Reference.java
@@ -15,7 +15,15 @@ class Student_Callref{
         this.email = email;
         this.contact = contact;
     }
-
+    Student_Callref(Student_Callref CR){
+        this.name = CR.name;
+        this.gen = CR.gen;
+        this.email = CR.email;
+        this.contact = CR.contact;
+    }
+    void displayCR(){
+        System.out.print(name+"\t"+gen+"\t"+email+"\t"+contact);
+    }
     void display(){
         System.out.print(enrol+"\t"+name+"\t"+fname+"\t"+gen+"\t"+age+"\t"+course+"\t"+batch+"\t"+email+"\t"+contact);
     }
@@ -25,7 +33,10 @@ class Call_Reference{
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter details: Enrol_No., Name, Father_Name, Gender, Age, Course, Batch, Email, Contact_No");
         Student_Callref obj1 = new Student_Callref(sc.nextInt(), sc.nextLine(),sc.nextLine(),sc.next(),sc.nextInt(),sc.next(), sc.nextInt(), sc.next(), sc.nextLong());
+        Student_Callref obj2 = new Student_Callref(obj1);
         System.out.println("Enrollment_No.\tStudent_Name\tFather_Name\tGender\tAge\tCourse\tBatch\tEmail\tContact_No.");
-            obj1.display();
+        obj1.display();
+        System.out.println("CR Details: ");
+        obj2.displayCR();
     }
 }
